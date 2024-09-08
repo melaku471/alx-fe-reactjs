9443eba964ac9be03eb0479dd9aeced9c83ff7c9
Melaku Girma <akuds260@gmail.com>
2024-09-08 11:20:11 -0700

    updated

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",               // Include the root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all files in src folder
+  ],
+  
+  darkMode: 'class', // or 'media' if you prefer system-based dark mode
+  
   theme: {
-    extend: {},
+    extend: {},  // You can extend the default theme here (e.g., colors, spacing, etc.)
   },
-  plugins: [],
+  
+  plugins: [],   // You can add Tailwind plugins here if needed
 }
-

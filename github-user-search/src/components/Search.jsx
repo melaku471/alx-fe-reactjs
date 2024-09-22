cccb2a4b48b78d761042dadf2c7876245a3c91c9
Melaku Girma <akuds260@gmail.com>
2024-09-22 06:14:55 -0700

    git search

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,4 +1,4 @@
-// src/components/Search.js
+// src/components/Search.jsx
 
 import React, { useState } from 'react';
 import { fetchUserData } from '../services/githubService';
@@ -19,7 +19,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError("Looks like we can't find the user");
     } finally {
       setLoading(false);
     }
